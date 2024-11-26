be0319a20bfaacea82950efcfae37aabc5b4620c
Tim Newsham <tim@fly.io>
2024-11-26 11:53:23 -1000

    add constructors

--- machines/api.go
+++ machines/api.go
@@ -1,10 +1,26 @@
 package machines
 
 import (
-       "github.com/superfly/coordBfaas/japi"
+	"github.com/superfly/coordBfaas/japi"
 )
 
 // MachinesApi provides a subset of the fly machines API.
 type Api struct {
-       json *japi.JsonApi
+	json *japi.JsonApi
+}
+
+// New returns a machines API for a specified url.
+func New(url string, opts ...japi.JsonApiOpt) *Api {
+	j := japi.New(url, opts...)
+	return &Api{j}
+}
+
+// NewPublic returns a machines API using the public url.
+func NewPublic(opts ...japi.JsonApiOpt) *Api {
+	return New("https://api.machines.dev", opts...)
+}
+
+// NewInternal returns a machines API using the internal url.
+func NewInternal(opts ...japi.JsonApiOpt) *Api {
+	return New("http://_api.internal:4280", opts...)
 }
